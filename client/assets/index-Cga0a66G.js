76f495580a7238a52ec3ac0da22e99d761b928f6
freight-capstone <mrepol742@gmail.com>
2024-09-30 13:49:54 +0000

    Production

---
+++ client/assets/index-Cga0a66G.js
@@ -0,0 +1,1 @@
+import{r as o,o as s,q as a,v as i}from"./index-CggOlxEd.js";const c=()=>{o.useState("");const e=s(),t=a.get(void 0);t===void 0&&e("/"),o.useEffect(()=>{r(),a.remove(void 0),e("/")},[]);async function r(){try{await i.post("undefined/api/auth/logout",{},{headers:{Authorization:`Bearer ${t}`}})}catch(n){console.error(n)}}return null};export{c as default};
