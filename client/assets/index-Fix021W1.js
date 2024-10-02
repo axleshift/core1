4ccd67d6da633e5406c9040fec7918fc5f2efe90
freight-capstone <mrepol742@gmail.com>
2024-10-02 07:48:01 +0000

    Production

---
+++ client/assets/index-Fix021W1.js
@@ -0,0 +1,1 @@
+import{r as a,q as i,v as e,y as n}from"./index-aI191j7q.js";const c=()=>{a.useState("");const t=i(),o=e.get("RCTSESSION");o===void 0&&t("/"),a.useEffect(()=>{r(),e.remove("RCTSESSION"),t("/")},[]);async function r(){try{await n.post("http://localhost:5050/api/auth/logout",{},{headers:{Authorization:`Bearer ${o}`}})}catch(s){console.error(s)}}return null};export{c as default};
