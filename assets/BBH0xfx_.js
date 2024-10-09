f08c489268665c9342aec673e5dad27b28ebacb6
freight-capstone <mrepol742@gmail.com>
2024-10-09 10:54:56 +0000

    Production

--- assets/BBH0xfx_.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./CZAm0P_r.js";import"./CaXXqTot.js";const o=()=>t.jsx(t.Fragment,{children:"Treat Detection"});export{o as default};
---
+++ assets/Bk9ZeTKX.js
@@ -0,0 +1,1 @@
+import"./CZAm0P_r.js";import{a as e}from"./NOUfOvi6.js";const i=()=>e(r=>r.user),n=()=>e(s=>s.user).role==="admin";export{i as P,n as i};
