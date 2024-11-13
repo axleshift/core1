6092167821513997c896f487ae082ace3aad8fd0
freight-capstone <mrepol742@gmail.com>
2024-11-13 18:01:50 +0000

    Production

--- src/Server.js
+++ src/Server.js
@@ -6,19 +6,27 @@ import multer from 'multer'
 import mongoSanitize from 'express-mongo-sanitize'
 import helmet from 'helmet'
 import compression from 'compression'
-import { NODE_ENV } from './config.js'
-
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import { NODE_ENV, EXT_EXPRESS_PORT } from './config.js'
 import rateLimiter from './middleware/rateLimiter.js'
 import sanitize from './middleware/sanitize.js'
-
 import logger from './components/logger.js'
-
 import APIv1 from './routes/v1/index.js'
 
 const app = express()
 const upload = multer()
 
 app.use(cors())
+app.use(
+    '/api/pmz',
+    createProxyMiddleware({
+        target: `http://localhost:${EXT_EXPRESS_PORT}`,
+        changeOrigin: true,
+        pathRewrite: {
+            '^/api/pmz': '',
+        },
+    }),
+)
 app.use(compression())
 app.use(
     mongoSanitize({
